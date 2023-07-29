35e5caf2fd26a2a212d418e321394ea9a1d22036
Develper Bishal Thapa <90990481+devbith@users.noreply.github.com>
2023-07-29 12:24:07 +0100

    Update script.sh

--- java/script.sh
+++ java/script.sh
@@ -2,14 +2,11 @@
 
 set -e
 
-j_directory_path=`echo ${1} | awk -F '/' -v OFS='/' '{$NF=""; print $0}' `
-
-j_file=`echo ${1} | awk -F '/' '{print $NF}' | sed 's/.java//'` 
-
-script_dir=`cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd`
+j_directory_path=$(dirname "$1")
+j_file=$(basename "$1" .java)
 
 # Compile and write result
-javac -cp ".:${j_directory_path}/*" ${1}
+javac -cp ".:${j_directory_path}/*" "$1"
 
 # Run and write result
-java -cp ".:${j_directory_path}/" ${j_file} 
+java -cp ".:${j_directory_path}" "$j_file"
